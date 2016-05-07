2ecdb3ad6bb6b5bf4583c52e007220ae890f3f65
Kevin Buisson <buisson.kevin2@gmail.com>
2016-05-07 19:30:20 +0200

    [Fix] Running scripts

--- scripts/runServer.sh
+++ scripts/runServer.sh
@@ -5,4 +5,4 @@ rmiregistry 8082 &
 cd ../
 
 # Run server
-java -Djava.rmi.server.useCodebaseOnly=false -Djava.security.policy="permissions.policy" -cp bin/ -Djava.rmi.server.codebase=http://localhost:2000/ collection.MainServer
+java -Djava.security.policy="permissions.policy" -cp bin/ collection.MainServer

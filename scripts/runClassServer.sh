5865f318cec2545b9866d17c36eba3486a3426ec
Kevin Buisson <buisson.kevin2@gmail.com>
2016-05-07 19:20:51 +0200

    [Fix] Classpath files to share

--- scripts/runClassServer.sh
+++ scripts/runClassServer.sh
@@ -1,2 +1,6 @@
+# Configuration
+PORT=2000
+SERVER_DIR=../Server/bin
+
 # Run class server
-java -Djava.security.policy="permissions.policy" -cp bin/ classserver.ClassFileServer
+java -Djava.security.policy="permissions.policy" -cp bin/ classserver.ClassFileServer $PORT $SERVER_DIR

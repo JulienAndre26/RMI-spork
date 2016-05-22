ceda10893529385f6f81bd2ecffb4fd2c747cd7b
Kevin Buisson <buisson.kevin2@gmail.com>
2016-05-22 16:59:09 +0200

    [Update] More dark magic

--- scripts/runClassServer.sh
+++ scripts/runClassServer.sh
@@ -1,6 +1,6 @@
 # Configuration
 PORT=2000
-SERVER_DIR=../Server/bin
+CODEBASE_DIRS="../Server/bin;../Client/bin"
 
 # Run class server
-java -Djava.security.policy="permissions.policy" -cp bin/ classserver.ClassFileServer $PORT $SERVER_DIR
+java -Djava.security.policy="permissions.policy" -cp bin/ classserver.ClassFileServer $PORT $CODEBASE_DIRS
--- scripts/runClient.sh
+++ scripts/runClient.sh
@@ -1,2 +1,3 @@
 # Run client
-java -Djava.rmi.server.useCodebaseOnly=false -Djava.security.policy="permissions.policy" -Djava.rmi.server.codebase=http://localhost:2000/ -cp bin/ client.MainClient
+# java -Djava.security.policy="permissions.policy" -Djava.rmi.server.useCodebaseOnly=false -Djava.rmi.server.codebase=http://localhost:2000/ -cp bin/ client.MainClient
+java -Djava.security.policy="permissions.policy" -cp bin/ client.MainClient # Without remote codebase system (working with rmiregistry but not with the client...)

e63e9402ba23b0e6465a811f2970ec7ea4c3e14e
Kevin Buisson <buisson.kevin2@gmail.com>
2016-05-22 16:58:56 +0200

    [Update] Some dark magic

--- deploy.sh
+++ deploy.sh
@@ -15,24 +15,26 @@ rm -rf $OUTPUT_DIR 2>/dev/null
 echo 'Deploy class server...'
 mkdir -p $CLASSSERVER_DIR/bin
 cp -R $BIN_DIR/classserver $CLASSSERVER_DIR/bin
-cp ./scripts/runClassServer.sh permissions.policy $CLASSSERVER_DIR
+cp ./scripts/runClassServer.sh permissions.policy $CLASSSERVER_DIR && chmod +x $CLASSSERVER_DIR/runClassServer.sh
 
 # Server
 echo 'Deploy server...'
 mkdir -p $SERVER_DIR/bin
 cp -R $BIN_DIR/collection $SERVER_DIR/bin
-cp ./scripts/runServer.sh permissions.policy $SERVER_DIR
+cp ./scripts/runServer.sh permissions.policy $SERVER_DIR && chmod +x $SERVER_DIR/runServer.sh
 
 # Client
 echo 'Deploy client...'
 mkdir -p $CLIENT_DIR/bin
 cp -R $BIN_DIR/client $CLIENT_DIR/bin
-cp ./scripts/runClient.sh permissions.policy $CLIENT_DIR
-cp -R $SERVER_DIR/bin/collection $CLIENT_DIR/bin
+cp ./scripts/runClient.sh permissions.policy $CLIENT_DIR && chmod +x $CLIENT_DIR/runClient.sh
+mkdir -p $CLIENT_DIR/bin/collection                              # Need to add the codebase, because for mysterious
+cp -R $BIN_DIR/collection/interfaces $CLIENT_DIR/bin/collection  # reason, rmi.codebase not working with the client
+cp -R $BIN_DIR/collection/database $CLIENT_DIR/bin/collection    # (but it does with rmiregistry)
 
 # Deploy RMI Registry script
 echo 'Deploy RMI Registry script...'
-cp ./scripts/runRMIRegistry.sh $OUTPUT_DIR/runRMIRegistry.sh
+cp ./scripts/runRMIRegistry.sh $OUTPUT_DIR/runRMIRegistry.sh && chmod +x $OUTPUT_DIR/runRMIRegistry.sh
 
 # Some informaitons
 echo "Deployment done in $OUTPUT_DIR"
